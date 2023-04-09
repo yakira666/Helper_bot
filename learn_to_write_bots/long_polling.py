bcb24a8a8ef0306903ad89397e5af4b5fa43002b
Kirill <zebzeeff.kirill@yandex.ru>
2023-04-10 01:01:53 +0300

    пуллинг и лонг пуллинг

---
+++ learn_to_write_bots/long_polling.py
@@ -0,0 +1,29 @@
+import os
+import time
+import requests
+
+from dotenv import load_dotenv
+
+load_dotenv("../.env")
+BOT_TOKEN: str = os.environ.get("API_TOKEN")
+API_URL: str = 'https://api.telegram.org/bot'
+offset: int = -2
+timeout: int = 50
+updates: dict
+
+
+def do_something() -> None:
+    print('Был апдейт')
+
+
+while True:
+    start_time = time.time()
+    updates = requests.get(f'{API_URL}{BOT_TOKEN}/getUpdates?offset={offset + 1}&timeout={timeout}').json()
+
+    if updates['result']:
+        for result in updates['result']:
+            offset = result['update_id']
+            do_something()
+
+    end_time = time.time()
+    print(f'Время между запросами к Telegram Bot API: {end_time - start_time}')
